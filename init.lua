7e68add81149c73a52ccaf84b2aa08b2ca899d34
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-04-17 23:57:34 -0300

    fix

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
 require('lucas.options')
 require('lucas.mapping')
 require('lucas.lazy')
+
+-- FIX: 
+-- integrar loa siguiente plagins
+-- ///////////////////////////////////////////////
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+-- este plugin remplasaria nvim-tree.lua 
+-- esta e una altenativa mas moderna
+-- //////////////////////////////////////////////
+-- https://github.com/willothy/nvim-cokeline
+-- este plugin es para las pestanas de los buffers
+-- alternativa
+-- https://github.com/nanozuki/tabby.nvim
+-- ///////////////////////////////////////////////
+-- https://github.com/Pocco81/auto-save.nvim/tree/main
+-- auto save
+-- ///////////////////////////////////////////////
+-- revisar plugins de lazyvim
+-- ///////////////////////////////////////////////
+
+
+
