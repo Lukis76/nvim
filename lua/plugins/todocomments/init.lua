62aa02eefeb6850e5dec3553ea5f96cccebec991
Lucas.R.Manoguerra <kiu.manoguerra@gmail.com>
2023-04-20 18:23:16 -0300

    ref require config todo comments

---
+++ lua/plugins/todocomments/init.lua
@@ -0,0 +1,28 @@
+return {
+    "folke/todo-comments.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    lazy = true,
+    cmd = { "TodoTrouble", "TodoTelescope", "TodoQuickFix" },
+    keys = {
+        {
+            "]t",
+            function()
+                require("todo-comments").jump_next()
+            end,
+            desc = "Jump to next TODO",
+        },
+        {
+            "[t",
+            function()
+                require("todo-comments").jump_prev()
+            end,
+            desc = "Jump to prev TODO",
+        },
+    },
+    config = function()
+        require("plugins.todocomments.config").setup()
+    end,
+}
