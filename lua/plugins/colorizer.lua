79ef4c40eeac1aa4b47526e9aa7b4ea76ab9977d
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-04-20 01:44:03 -0300

    colorizer reload

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,5 +1,7 @@
 return {
-  "norcalli/nvim-colorizer.lua",
-  event = "VeryLazy",
-  config = true,
+    "norcalli/nvim-colorizer.lua",
+    event = "VeryLazy",
+    config = function()
+        require("colorizer").setup()
+    end,
 }
