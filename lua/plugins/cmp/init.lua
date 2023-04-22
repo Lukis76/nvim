187f9d820235ec5d5b43fa509841368b867fb8a7
Lucas.R.Manoguerra <kiu.manoguerra@gmail.com>
2023-04-22 12:14:02 -0300

    .

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -3,11 +3,15 @@ return {
     event = "VeryLazy",
     -- Required
     dependencies = {
-        { "hrsh7th/cmp-nvim-lsp" },     -- Required
-        { "hrsh7th/cmp-buffer" },       -- Optional
-        { "hrsh7th/cmp-path" },         -- Optional
-        { "saadparwaiz1/cmp_luasnip" }, -- Optional
-        { "hrsh7th/cmp-nvim-lua" },     -- Optional
+        { "hrsh7th/cmp-nvim-lsp" },         -- Required
+        { "hrsh7th/cmp-buffer" },           -- Optional
+        { "hrsh7th/cmp-path" },             -- Optional
+        { "saadparwaiz1/cmp_luasnip" },     -- Optional
+        { "hrsh7th/cmp-nvim-lua" },         -- Optional
+        { "L3MON4D3/LuaSnip" },             -- Optional
+        { "rafamadriz/friendly-snippets" }, -- Optional
+        {"hrsh7th/cmp-cmdline"},        -- Optional
+
     },
     config = function()
         require("plugins.cmp.config").setup()
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -16,6 +16,7 @@ return {
         { "hrsh7th/cmp-path" },         -- Optional
         { "saadparwaiz1/cmp_luasnip" }, -- Optional
         { "hrsh7th/cmp-nvim-lua" },     -- Optional
+        {"hrsh7th/cmp-cmdline"},        -- Optional
 
         -- Snippets
         { "L3MON4D3/LuaSnip" },             -- Required
